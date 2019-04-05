0f38728bdaf32e648322c20f47dc8d91eea8ebd2
Jasterix C <loune.calixte@gmail.com>
2019-04-05 01:38:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,3 +11,20 @@ def line(katz_deli)
     puts "The line is currently empty."
   end
 end
+
+#take a number
+def take_a_number(katz_deli, name)
+	katz_deli.push(name)
+	puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+#	end
+end
+
+#now serving
+def now_serving(katz_deli)
+  if katz_deli == 0
+    puts "There is nobody waiting to be served!"
+  else 
+	 puts "Currently serving " + katz_deli[0] +"."
+  end
+  katz_deli.shift
+end
