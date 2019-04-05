234922f2804a65b629fbd8647e205b30b31929aa
Jasterix C <loune.calixte@gmail.com>
2019-04-05 01:53:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,4 +24,8 @@ def now_serving(katz_deli)
     puts "Currently serving #{katz_deli[0]}."
   else katz_deli == 0
     puts "There is nobody waiting to be served!"
+  # else 
+	 #puts "Currently serving #{katz_deli[0]}."
+  end
+  katz_deli.shift
 end
